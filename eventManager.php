ebb77eab0fb4c7f3509950228d79fc4fb19f0de9
alext114 <alext1144@gmail.com>
2017-08-03 18:53:25 -0400

    Update eventManager.php

--- eventManager.php
+++ eventManager.php
@@ -15,7 +15,9 @@
     }
 
     function placeInQueue(){
-
+        $queryEvents = ;//INSERT INTO events_queue (`fullName`,`emailAddress`,`phoneNumber`,`eventDate`,`movie`,`eventTime`,`rate`,`numOfPeople`,`specialAttention`,`eventType`,`depositAmt`,`receivedDeposit`,`partyRoomBook`,`childName`,`isApproved`,`theater`) WHERE (`fullName`,`emailAddress`,`phoneNumber`,`eventDate`,`movie`,`eventTime`,`rate`,`numOfPeople`,`specialAttention`,`eventType`,`depositAmt`,`receivedDeposit`,`partyRoomBook`,`childName`,`isApproved`,`theater`)
+        $result = $db->prepare($queryEvents);
+        $result->execute(); 
     }
 
     function createEvent(){
