2cd7afce937f9882ff0ee798627981c31ad5a200
alext114 <alext1144@gmail.com>
2017-08-03 20:26:51 -0400

    Update eventManager.php

--- eventManager.php
+++ eventManager.php
@@ -16,7 +16,7 @@
 
     function placeInQueue($submittedEvent){
         
-        $queryEvents = ;//INSERT INTO events_queue ($submittedEvent['fullName'],$submittedEvent['emailAddress'],$submittedEvent['phoneNumber'],$submittedEvent['eventDate'],$submittedEvent['movie'],$submittedEvent['eventTime'],$submittedEvent['rate'],$submittedEvent['numOfPeople'],$submittedEvent['specialAttention'],$submittedEvent['eventType'],`depositAmt`,`receivedDeposit`,`partyRoomBook`,`childName`,`isApproved`,`theater`) WHERE (`fullName`,`emailAddress`,`phoneNumber`,`eventDate`,`movie`,`eventTime`,`rate`,`numOfPeople`,`specialAttention`,`eventType`,`depositAmt`,`receivedDeposit`,`partyRoomBook`,`childName`,`isApproved`,`theater`)
+        $queryEvents = INSERT INTO events_queue ($submittedEvent['fullName'],$submittedEvent['emailAddress'],$submittedEvent['phoneNumber'],$submittedEvent['eventDate'],$submittedEvent['movie'],$submittedEvent['eventTime'],$submittedEvent['rate'],$submittedEvent['numOfPeople'],$submittedEvent['specialAttention'],$submittedEvent['eventType'],$submittedEvent['depositAmt'],$submittedEvent['receivedDeposit'],$submittedEvent['partyRoomBook'],$submittedEvent['childName'],$submittedEvent['isApproved'],$submittedEvent['eventType'],$submittedEvent['theater']) WHERE (`fullName`,`emailAddress`,`phoneNumber`,`eventDate`,`movie`,`eventTime`,`rate`,`numOfPeople`,`specialAttention`,`eventType`,`depositAmt`,`receivedDeposit`,`partyRoomBook`,`childName`,`isApproved`,`theater`);
         $result = $db->prepare($queryEvents);
         $result->execute(); 
     }
