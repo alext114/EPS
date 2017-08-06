21fc69f0119585b40b26ce5c2ced3146bab160fd
alext114 <alext1144@gmail.com>
2017-08-06 13:25:13 -0400

    Add files via upload
    finished most of the methods

--- eventManager.php
+++ eventManager.php
@@ -5,8 +5,54 @@
     var $eventID
 
 
-    function checkAvailabilty($date){
+    function checkAvailabilty($wantsPartyRoom, $date){
+    $systemDate=date("Y-m-d");
+    $diffOfDates= date_diff($systemDate,$date);
+    $isBooked=false; //declaring variable
+    //sql statement that retrieves partyRoomBook boolean from the given datee
 
+    $query = mysqli_query($db, "SELECT partyRoomBook FROM events WHERE date='$date'");
+    //num_rows returns number of rows returned from query statement
+    $numOfEvents = mysqli_num_rows($query);
+    //3 is the max amount of events in a day
+    if (numOfEvents >= 3){
+        return false;
+    }
+    else{
+        //test to see if new event requires party room, if true it mmust now check if any other event on that day has the party room already booked
+        if($wantsPartyRoom== true){
+            //$query is the query stated above
+            while($row = mysqli_fetch_assoc($query)){
+                //if true isBooked is set to true else false
+                if($row['partyRoomBook']==true){
+                    isBooked=true;
+                    break;
+                }
+                else
+                {
+                    isBooked=false
+                }//end partyRooomBook if
+            }//end while
+            if (isBooked==true){
+                //room is booked, return false, about availiabilty
+                return false;
+
+                    
+            }
+            else{
+                //if the timespan between the dates is over 30, warning message is given.
+                if($diffOfDates>=30){
+                echo "Movie might not be available if booking a month in advance. Please finish booking and then call the manager at the movie theater you are booking at to verify. "                    
+                return true;
+                }
+            }//end isBooked if
+        }//end $wantsPartyRoom if
+    }
+     //if the timespan between the dates is over 30, warning message is given.    
+    if($diffOfDates>=30){
+    echo "Movie might not be available if booking a month in advance. Please finish booking and then call the manager at the movie theater you are booking at to verify. "                    
+    return true;
+    }   
 
     }
 
