ea2bf8a14a4af153a35c9a6f92ebd3e0adacc9eb
alext114 <alext1144@gmail.com>
2017-08-16 00:57:02 -0400

    notes for sly in acceptedEvents

--- acceptedevents.php
+++ acceptedevents.php
@@ -1,6 +1,28 @@
+<?php
+include 'dbh.php';
+include 'manager.php';
+//create manager and get an array of all the events
+$manager=new manager();
+$event=$manager->viewAccepted($db);
+
+
+//Notes for Sly
+/*
+make the green and blue for buttons brigher
+change deposit paid to event information
+get all the information to line up
+bold the labels i.e: Name: <--- that is bolded
+make the font bigger
+
+
+
+*/
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
+
     <meta content="text/html; charset=utf-8" http-equiv="content-type">
     <title>acceptedeventsPage</title>
     <style>
@@ -10,38 +32,38 @@
   </style>
   </head>
   <body>
+
     <form method="POST" action="logout.php">
       <div style="text-align: right;">
         <link rel="stylesheet" type="text/css" href="ButtonReferences.css">
         <button type="button" name="logoutButton" class="logoutButton">Logout</button></div>
     </form>
     <link rel="stylesheet" type="text/css" href="ButtonReferences.css">
-    <form method="POST" action="dayshift.php"> <button type="button" name="prevEntryButton"
-        class="prevButton">Previous Entry</button>&nbsp;&nbsp;
+    <button type="button" id="prevButton" class="prevButton" onclick="prevEvent()">Previous Entry</button>&nbsp;&nbsp;
       <link rel="stylesheet" type="text/css" href="ButtonReferences.css">
-      <button type="button" name="nextEntryButton" class="nextButton"> Next
+      <button type="button" id="nextButton" class="nextButton" onclick="nextEvent()" > Next
         Entry</button></form>
     <form method="POST" action="editEvent.php">
       <div style="text-align: left;"><br>
-        <div style="text-align: right;">Deposit Paid <input name="depositCheckBox"
-            type="checkbox"></div>
+        <div style="text-align: right;">Deposit Paid <input name="depositCheckBox" type="checkbox"></div>
         <br>
         <fieldset name="personalInfoFieldSet"> Name:&nbsp;&nbsp; &nbsp;&nbsp;
           &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
           &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <label form="nameLabel">-name-</label>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <label id="fullName"> </label>
           <p style="text-align: left;">E-Mail: &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
             &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;
             &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
-            &nbsp;&nbsp;&nbsp; <label form="emailLabel">-email-</label> &nbsp;
+            &nbsp;&nbsp;&nbsp; <label id="emailAddress"></label> &nbsp;
             &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
             &nbsp;&nbsp;&nbsp;&nbsp; <br>
           </p>
           <p style="text-align: left;">Phone Number:&nbsp;&nbsp;&nbsp;
             &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
-            &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; <label form="phoneLabel">XXX-XXX-XXXX</label></p>
-          <div style="text-align: left;">Child's Name (If Applicable):&nbsp; <label
-              form="childnameLabel">-billy-</label> &nbsp; </div>
+            &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; <label id="phoneNumber"></label></p>
+          <div style="text-align: left;">Child's Name (If Applicable):&nbsp; <label
+
+              id="childName"></label> &nbsp; </div>
           <legend>Personal Information</legend></fieldset>
         <legend></legend></div>
     </form>
@@ -49,50 +71,48 @@
     <fieldset name="theaterFieldSet"><legend>Theater Information</legend>Theater:&nbsp;&nbsp;
       &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
       &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
-      &nbsp;&nbsp;&nbsp; &nbsp;&nbsp; <label form="theaterNameLabel">-theater
-        name-</label>
+      &nbsp;&nbsp;&nbsp; &nbsp;&nbsp; <label id="theater"></label>
       <p>Movie: &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
         &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp; &nbsp;&nbsp;
         &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <label form="movieNameLabel">dat
-          movie son</label> &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <label id="movie"></label>
+        &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
         &nbsp;&nbsp;&nbsp; &nbsp;&nbsp; <br>
       </p>
       <p>Date Desired:&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
         &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <label form="dateLabel">XX/XX/XXXX</label><label></label></p>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <label id="eventDate"></label></p>
       Time: &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
       &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
       &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
-      &nbsp;&nbsp;&nbsp; &nbsp; &nbsp; <label form="timeLabel">11:11 PM</label>
-      &nbsp; <legend></legend></fieldset>
+      &nbsp;&nbsp;&nbsp; &nbsp; &nbsp; <label id="eventTime"></label>
+      &nbsp; </fieldset>
     &nbsp; &nbsp; <br>
     <fieldset name="eventinfoFieldSet"><legend>Event Information</legend>Event
       Type:&nbsp;&nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
       &nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
-      &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp; <label form="eventTypeLabel">-event
-        type-</label>
+      &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp; <label id="eventType"></label>
       <p>People Attending: &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
         &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp; &nbsp;&nbsp;
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <label form="numberAttendingLabel">XXX</label><br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <label id="numOfPeople"></label><br>
       </p>
       <p>Party Room:&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
         &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <label form="partyRoomConfirmationLabel">Yes/No</label><label></label></p>
+        <label id="partyRoomBook"></label></p>
       Party Room Time: &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
       &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
-      &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; <label form="partyRoomTimeLabel">11:12
-        PM</label> &nbsp; </fieldset>
+      &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; <label id="partyRoomTime"></label> &nbsp; </fieldset>
     <br>
     <fieldset name="additionalInfoFieldSet"><legend>Additional Information</legend>Brief
-      Description:<br>
+      Description:
       <br>
-      <label form="descLabel">Description Here</label><br>
+      <label id="description"></label>
       <br>
-      Special Attention:<br>
       <br>
-      <label form="specialneedsLabel">Needs Here</label></fieldset>
+      Special Attention:
+      <br>
+      <label id="specialAttention"></label></fieldset>
     <br>
     <br>
     <br>
@@ -105,5 +125,91 @@
         <link rel="stylesheet" type="text/css" href="ButtonReferences.css">
         <button type="button" name="backButton" class="backButton">Back</button></div>
     </form>
+    <script>
+
+    //declare the index var
+     var index=0;
+    //pass php array to javascript
+    var acceptedEvent= <?php echo json_encode($event); ?>;
+    //change content to top of array
+    document.getElementById("fullName").innerHTML = acceptedEvent[index]['fullName'];
+    document.getElementById("emailAddress").innerHTML = acceptedEvent[index]['emailAddress'];
+    document.getElementById("phoneNumber").innerHTML = acceptedEvent[index]['phoneNumber'];
+    document.getElementById("childName").innerHTML = acceptedEvent[index]['childName'];
+    document.getElementById("theater").innerHTML = acceptedEvent[index]['theater'];
+    document.getElementById("movie").innerHTML = acceptedEvent[index]['movie'];
+    document.getElementById("eventDate").innerHTML = acceptedEvent[index]['eventDate'];
+    document.getElementById("eventTime").innerHTML = acceptedEvent[index]['eventTime'];
+    document.getElementById("eventType").innerHTML = acceptedEvent[index]['eventType'];
+    document.getElementById("numOfPeople").innerHTML = acceptedEvent[index]['numOfPeople'];
+    document.getElementById("partyRoomBook").innerHTML = acceptedEvent[index]['partyRoomBook'];
+    document.getElementById("partyRoomTime").innerHTML = acceptedEvent[index]['partyRoomTime'];
+    document.getElementById("description").innerHTML = acceptedEvent[index]['description'];
+    document.getElementById("specialAttention").innerHTML = acceptedEvent[index]['specialAttention'];
+
+
+      function nextEvent(){
+        //if the index is less than amount of events, continue
+
+        if (index < acceptedEvent.length-1){
+          document.getElementById("prevButton").disabled = false;
+            index++;
+
+            document.getElementById("fullName").innerHTML = acceptedEvent[index]['fullName'];
+            document.getElementById("emailAddress").innerHTML = acceptedEvent[index]['emailAddress'];
+            document.getElementById("phoneNumber").innerHTML = acceptedEvent[index]['phoneNumber'];
+            document.getElementById("childName").innerHTML = acceptedEvent[index]['childName'];
+            document.getElementById("theater").innerHTML = acceptedEvent[index]['theater'];
+            document.getElementById("movie").innerHTML = acceptedEvent[index]['movie'];
+            document.getElementById("eventDate").innerHTML = acceptedEvent[index]['eventDate'];
+            document.getElementById("eventTime").innerHTML = acceptedEvent[index]['eventTime'];
+            document.getElementById("eventType").innerHTML = acceptedEvent[index]['eventType'];
+            document.getElementById("numOfPeople").innerHTML = acceptedEvent[index]['numOfPeople'];
+            document.getElementById("partyRoomBook").innerHTML = acceptedEvent[index]['partyRoomBook'];
+            document.getElementById("partyRoomTime").innerHTML = acceptedEvent[index]['partyRoomTime'];
+            document.getElementById("description").innerHTML = acceptedEvent[index]['description'];
+            document.getElementById("specialAttention").innerHTML = acceptedEvent[index]['specialAttention'];
+
+
+
+      }
+      //else disable the next button to prevent the user from going further
+      else{
+        document.getElementById("nextButton").disabled = true;
+
+
+      }
+      }
+      function prevEvent(){
+        // if the index is equal to 0, disable prevButton abd enable next button
+        if (index <= 0){
+          document.getElementById("nextButton").disabled = false;
+          document.getElementById("prevButton").disabled = true;
+        }
+        else{
+          //else index moves back
+          document.getElementById("nextButton").disabled = false;
+            index--;
+            document.getElementById("fullName").innerHTML = acceptedEvent[index]['fullName'];
+            document.getElementById("emailAddress").innerHTML = acceptedEvent[index]['emailAddress'];
+            document.getElementById("phoneNumber").innerHTML = acceptedEvent[index]['phoneNumber'];
+            document.getElementById("childName").innerHTML = acceptedEvent[index]['childName'];
+            document.getElementById("theater").innerHTML = acceptedEvent[index]['theater'];
+            document.getElementById("movie").innerHTML = acceptedEvent[index]['movie'];
+            document.getElementById("eventDate").innerHTML = acceptedEvent[index]['eventDate'];
+            document.getElementById("eventTime").innerHTML = acceptedEvent[index]['eventTime'];
+            document.getElementById("eventType").innerHTML = acceptedEvent[index]['eventType'];
+            document.getElementById("numOfPeople").innerHTML = acceptedEvent[index]['numOfPeople'];
+            document.getElementById("partyRoomBook").innerHTML = acceptedEvent[index]['partyRoomBook'];
+            document.getElementById("partyRoomTime").innerHTML = acceptedEvent[index]['partyRoomTime'];
+            document.getElementById("description").innerHTML = acceptedEvent[index]['description'];
+            document.getElementById("specialAttention").innerHTML = acceptedEvent[index]['specialAttention'];
+
+
+        }
+
+      }
+
+    </script>
   </body>
 </html>
--- eventManager.php
+++ eventManager.php
@@ -1,38 +1,40 @@
 <?php
 
-  public class eventManager{
+   class eventManager{
 
-    public $eventID
+    public $eventID;
 
 
     public function checkAvailabilty($db, $wantsPartyRoom, $date){
-    $systemDate=date("Y-m-d");
-    $diffOfDates= date_diff($systemDate,$date);
+    $systemDate=date_create();
+    $newDate= date_create($date);
+    $diffOfDates=  date_diff($systemDate,$newDate);
     $isBooked=false; //declaring variable
+    //$numOfEvents=0;
     //sql statement that retrieves partyRoomBook boolean from the given datee
 
-    $query ="SELECT partyRoomBook FROM events WHERE date='$date'");
+    $query ="SELECT partyRoomBook FROM events WHERE date='$date'";
     $result=$db->query($query);
 
     //num_rows returns number of rows returned from query statement
     $numOfEvents = $result->num_rows;
+    echo $numOfEvents;
     //3 is the max amount of events in a day
     if ($numOfEvents >= 3){
         return false;
     }
     else{
         //test to see if new event requires party room, if true it mmust now check if any other event on that day has the party room already booked
-        if($wantsPartyRoom== true){
+        if($wantsPartyRoom== 'Yes'){
             //$query is the query stated above
             while($row = $result->fetch_assoc()){
                 //if true isBooked is set to true else false
-                if($row['partyRoomBook']==true){
+                if($row['partyRoomBook']=='Yes'){
                     $isBooked=true;
                     break;
                 }
-                else
-                {
-                    $sBooked=false
+                else{
+                    $isBooked=false;
                 }//end partyRooomBook if
             }//end while
             if ($isBooked==true){
@@ -44,7 +46,7 @@
             else{
                 //if the timespan between the dates is over 30, warning message is given.
                 if($diffOfDates>=30){
-                echo "Movie might not be available if booking a month in advance. Please finish booking and then call the manager at the movie theater you are booking at to verify. "
+                echo "Movie might not be available if booking a month in advance. Please finish booking and then call the manager at the movie theater you are booking at to verify. ";
                 return true;
                 }
             }//end isBooked if
@@ -52,7 +54,7 @@
     }
      //if the timespan between the dates is over 30, warning message is given.
     if($diffOfDates>=30){
-    echo "Movie might not be available if booking a month in advance. Please finish booking and then call the manager at the movie theater you are booking at to verify. "
+    echo "Movie might not be available if booking a month in advance. Please finish booking and then call the manager at the movie theater you are booking at to verify. ";
     return true;
     }
 
@@ -64,9 +66,23 @@
 
     function placeInQueue($submittedEvent){
 
-        $queryEvents = "INSERT INTO events_queue ($submittedEvent['fullName'],$submittedEvent['emailAddress'],$submittedEvent['phoneNumber'],$submittedEvent['eventDate'],$submittedEvent['movie'],$submittedEvent['eventTime'],$submittedEvent['rate'],$submittedEvent['numOfPeople'],$submittedEvent['specialAttention'],$submittedEvent['eventType'],$submittedEvent['depositAmt'],$submittedEvent['receivedDeposit'],$submittedEvent['partyRoomBook'],$submittedEvent['childName'],$submittedEvent['isApproved'],$submittedEvent['eventType'],$submittedEvent['theater']) WHERE (`fullName`,`emailAddress`,`phoneNumber`,`eventDate`,`movie`,`eventTime`,`rate`,`numOfPeople`,`specialAttention`,`eventType`,`depositAmt`,`receivedDeposit`,`partyRoomBook`,`childName`,`isApproved`,`theater`);"
-        $db->query($queryEvents);
-        $db->close();
+      $queryEvents = "INSERT INTO events_queue (`eventID`, `fullName`, `emailAddress`, `phoneNumber`, `eventDate`, `description`, `movie`, `eventTime`, `rate`, `numOfPeople`, `specialAttention`, `eventType`, `depositAmt`, `recievedDeposit`, `partyRoomBook`, `childName`, `isApproved`, `theater`)
+      VALUES (null, '$fullName', '$emailAddress', '$phoneNumber', '$eventDate', '$description', '$movie', '$eventTime', null, '$numOfPeople', '$specialAttention', '$eventType', null, null, '$partyRoomBook', '$childName', 1, '$theater')";
+      //dont forget to fix null VALUES
+      if ($db->query($queryEvents) === TRUE)
+      {
+        print '<script>alert("Your Event is now in wait for approval by the selected theater ");</script>';
+       //redirects to home.html
+      //  print '<script>window.location.assign("confirmation.php");</script>';
+      }
+      else {
+          $error= "Error: " . $queryEvents . "<br>" . $db->error;
+          //print '<script>alert('$error');</script>';
+          //redirects to home.html
+          print '<script>window.location.assign("home.html");</script>';
+
+      }
+      $db->close();
     }
 
     function createEvent(){
