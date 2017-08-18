89ae4a6736791b325cd8268f90136c02eff595ce
alext114 <alext1144@gmail.com>
2017-08-17 20:45:17 -0400

    we got a celndar now bois

---
+++ home.php
@@ -0,0 +1,76 @@
+<?php
+session_start();
+
+
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta content="text/html; charset=utf-8" http-equiv="content-type">
+    <title>EPS Home</title>
+    <style>
+    body{
+      background-color: #98FB98
+    }
+  </style>
+  </head>
+  <body>
+    <div style="text-align: center;">Hello <label form="nameLabel"><?php echo $_SESSION['username'];?></label>
+      !<br>
+      <h2 style="text-align: center;"><img style="width: 64px; height: 79px;" src="http://images.clipartpanda.com/movie-clipart-popcorn3.png">Event
+        Popper System<img style="width: 64px; height: 79px;" src="http://images.clipartpanda.com/movie-clipart-popcorn3.png"></h2>
+
+      <br>
+      <iframe src="https://calendar.google.com/calendar/embed?src=33fk84kf0rdcqb7frkm8f1ocl0%40group.calendar.google.com&ctz=America/New_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+      <form method="POST" action="quicksearch.php">
+        <div style="text-align: right;"><input name="eventDateTextBox" type="date"></div>
+        <div style="text-align: left;"> <br>
+          <div style="text-align: right;">
+            <link rel="stylesheet" type="text/css" href="ButtonReferences.css">
+            <button type="button" name="searchButton" class="searchButton" href="ButtonReferences.css">
+              Quick Search</button></div>
+        </div>
+      </form>
+
+      <form method="POST" action="namesearch.php">
+        <div style="text-align: center;"><input name="namesearchTextBox" type="text">
+          <link rel="stylesheet" type="text/css" href="ButtonReferences.css">
+          <button type="button" name="namesearchButton" class="searchNameButton">
+            Name Search</button></div>
+      </form>
+      <br>
+
+      <div>
+        <div style="text-align: center;">
+          <link rel="stylesheet" type="text/css" href="ButtonReferences.css">
+          <button type="button" name="pendingEventButton" class="pendingButton"
+
+            onclick="window.location.href='pendingevents.php';"> Manage Pending
+            Events</button>
+          <link rel="stylesheet" type="text/css" href="ButtonReferences.css">
+          <button type="button" name="acceptedEventButton" class="acceptedButton"
+
+            onclick="window.location.href='acceptedevents.php';"> Manage
+            Accepted Events</button> </div>
+      </div>
+      <br>
+      <link rel="stylesheet" type="text/css" href="ButtonReferences.css">
+      <a href="managersubmissionform.html" name="createButton" class="changeButton">
+        Create Event</a>
+      <link rel="stylesheet" type="text/css" href="ButtonReferences.css">
+      <button type="button" name="depositcheckerButton" class="checkButton" onclick="window.location.href='depositchecker.html';">
+        Check Unpaid Deposits</button> </div>
+    <br>
+    <br>
+    <div style="text-align: justify;">
+      <link rel="stylesheet" type="text/css" href="ButtonReferences.css">
+      <a href="manage.html" class="editButton"> Manage Accounts</a> <br>
+      <form method="POST" action="logout.php">
+        <div style="text-align: right;">
+          <link rel="stylesheet" type="text/css" href="ButtonReferences.css">
+          <button type="button" name="logoutButton" class="logoutButton"> Logout</button></div>
+      </form>
+    </div>
+
+  </body>
+</html>
